142b328fcfac66e445e4d7e90f15f1e9843d8440
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-12-02 20:55:22 -0800

    installed dotenv npm package and achieved first bot function when executing bot.js file

--- bot.js
+++ bot.js
@@ -1,2 +1,10 @@
+require('dotenv').config()
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('kevin-bot is ready to serve!')
+})
+
+client.login(process.env.BOT_TOKEN)
