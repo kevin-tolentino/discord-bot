4ddedf0edc9230158edce58869df3d44dd5529d7
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-12-02 21:43:09 -0800

    installed eslint and nodemon for automated server. Also added feature to react and reply to certain message

--- bot.js
+++ bot.js
@@ -4,7 +4,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('kevin-bot is ready to serve!')
+  console.log('kevin-bot is ready to serve!!!')
 })
+
+client.on('message', msg => {
+  if (msg.content === 'Jesus loves you!') {
+    msg.react('😃');
+    msg.react('💖');
+    msg.reply(` wow! Jesus loves you too! 💖`);
+  }
+});
 
 client.login(process.env.BOT_TOKEN)
